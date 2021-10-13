618ee7af27d808aab4e34a614302863e1ec7d76e
nishubharti <32607124+nishubharti@users.noreply.github.com>
2021-10-13 17:48:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,8 @@ data "template_file" "test" {
 }
 
 module "tf_module_test" {
-  source = "https://cm.globalcatalog.test.cloud.ibm.com/api/v1-beta/catalogs/b339e42a-44d3-472f-be3d-7b66a08ba67f/offerings/893dcc32-b6dc-41f7-858f-af905e20e993/source?archive=tgz&kind=terraform&version=1.5.4"
+  //source = "https://cm.globalcatalog.test.cloud.ibm.com/api/v1-beta/catalogs/b339e42a-44d3-472f-be3d-7b66a08ba67f/offerings/893dcc32-b6dc-41f7-858f-af905e20e993/source?archive=tgz&kind=terraform&version=1.5.4"
+   source = "https://schematics.test.cloud.ibm.com/v1/workspaces"
  //source = "https://dev-cm.globalcatalog.test.cloud.ibm.com/api/v1-beta/catalogs/54b7df75-df03-44dc-95c5-06822d514035/offerings/08a3aaab-54af-48e9-8285-b34451c7cc2b/source?archive=tgz&kind=terraform&version=0.0.1"
  module_sample_var = "************** ${var.sample_var} ********** "
 }
